cb5a71d6483d21c4240e84919c7d1fabc63159bd
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-12-03 00:19:29 +0530

    Add files via upload

--- malloc_operation.c
+++ malloc_operation.c
@@ -10,11 +10,9 @@ int main()
 	//creating array of 100 float elements
 	float_100 = (float*)calloc(100,sizeof(float));
 	
-	printf("The value of array\n");
 	//Assiging value to the array
 	for(int i=0;i<100;i++)
 	{
-		printf("%f\n",float_100[i]);
 		float_100[i] = (i+1)*(i+1);
 	}
 	mean_varience(float_100,float_2,n);
@@ -23,6 +21,34 @@ int main()
 	{
 		printf("%f\n",float_2[i]);
 	}
+	
+	FILE *txt_pointer ; 
+      
+   
+      
+   txt_pointer =fopen("txt_output.txt", "w") ; 
+      
+
+    if ( txt_pointer == NULL ) 
+    { 
+        printf( "txt_output.txt file failed to open." ) ; 
+    } 
+    else
+    { 
+          
+        printf("The txt file is now opened to write.\n") ; 
+          
+        
+       // writing in the file
+        fprintf(txt_pointer, "%f \n %f\n", float_2[0],float_2[1]);
+            
+       // Closing the file 
+       fclose(txt_pointer) ; 
+          
+        printf("Output written sucessfully in txt_output.txt\n"); 
+        printf("The file is closed.\n") ; 
+    } 
+  
 	free (float_100);
 	return(0);
 }
