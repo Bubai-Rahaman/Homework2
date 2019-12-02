e861f0a53018ae11ef22bf257982df97ce65eb85
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-12-02 23:49:35 +0530

    Add files via upload

---
+++ data_type_size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//size of char
+	printf("Size of 'char' is %ld bytes\n",sizeof(char));
+	
+	//size of int
+	printf("Size of 'int' is %ld bytes\n",sizeof(int));
+	
+	//size of long
+	printf("Size of 'long' is %ld bytes\n",sizeof(long));
+	
+	//size of long int
+	printf("Size of 'long int' is %ld bytes\n",sizeof(long int));
+	
+	//size of float
+	printf("Size of 'float' is %ld bytes\n",sizeof(float));
+	
+	//size of double
+	printf("Size of 'double' is %ld bytes\n",sizeof(double));
+
+}
